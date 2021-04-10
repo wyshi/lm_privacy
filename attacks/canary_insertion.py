acbbeabbc7598b812bf64bb7b589a321d51a21ff
wyshi <wyshi@berkeley.edu>
2021-04-10 15:32:00 -0700

    changes

--- attacks/canary_insertion.py
+++ attacks/canary_insertion.py
@@ -17,9 +17,11 @@ from scipy.integrate import quad
 import scipy.stats
 import numpy as np
 
+import pandas as pd
+
 '''
-about 5 mins for 6-digit canary " My SSN is 341752."
-python attacks/canary_insertion.py -bs 1024 --checkpoint model/nodp/20210408/223716/data-wikitext-2-add10b__model-LSTM__ebd-200__hid-200__bi-False__nlayer-1__tied-False__ntokens-50258__bs-256__bptt-35__lr-20.0__dp-False_partial-False.pt 
+about 5 mins for 6-digit canary " My SSN is 341752." for one model
+python attacks/canary_insertion.py -bs 256 --checkpoint model/nodp/20210409/185850 --outputf attacks/canary_insertion/nodp_10insertion.csv
 '''
 CANARY = " My SSN is 341752."
 TOTAL_CANDIDATES = 1000_000
@@ -58,75 +60,28 @@ class CanaryDataset(Dataset):
         return unpacked_data
 
 def estimate(pp, low, high):
-      #----------------------------------------------------------------------------------------#
-  # Normal Distribution
+    #----------------------------------------------------------------------------------------#
+    # Normal Distribution
 
-  mean = (low + high)/2
-  std = 3.0
+    mean = (low + high)/2
+    std = 3.0
 
-  x = np.linspace(low, high, 10000)
+    x = np.linspace(low, high, 10000)
 
-  y = scipy.stats.norm.pdf(x,mean,std)
+    y = scipy.stats.norm.pdf(x,mean,std)
 
-  #----------------------------------------------------------------------------------------#
+    #----------------------------------------------------------------------------------------#
 
-  def normal_distribution_function(x):
-      value = scipy.stats.norm.pdf(x,mean,std)
-      return value
+    def normal_distribution_function(x):
+        value = scipy.stats.norm.pdf(x,mean,std)
+        return value
 
-  res, err = quad(normal_distribution_function, 0, pp)
+    res, err = quad(normal_distribution_function, 0, pp)
 
-  return math.log(res, 2) * -1
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 Language Model')
-
-    # Model parameters.
-    parser.add_argument('--checkpoint', '-ckpt', type=str, default='/home/wyshi/privacy/model/nodp/model-LSTM__ebd-200__hid-200__bi-False__nlayer-1__tied-False__ntokens-33278__bs-256__bptt-35__lr-20.0__dp-False.pt',
-                        help='model checkpoint to use')
-    parser.add_argument('--outf', type=str, default='attacks/canary_sorted_ppl.json',
-                        help='output file for generated text')
-    parser.add_argument('--seed', type=int, default=1111,
-                        help='random seed')
-    parser.add_argument('--batch_size', '-bs', type=int, default=16,
-                        help='batch size')
-    parser.add_argument('--cuda', type=str, default="cuda:0",
-                        help='use CUDA')
-    args = parser.parse_args()
+    return math.log(res, 2) * -1
 
 
-    # Set the random seed manually for reproducibility.
-    torch.manual_seed(args.seed)
-    if torch.cuda.is_available():
-        if not args.cuda:
-            print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-
-    device = torch.device(args.cuda)
-
-    ###############################################################################
-    # Load model
-    ###############################################################################
-    with open(args.checkpoint, 'rb') as f:
-        model = torch.load(f, map_location=device)
-    model.eval()
-
-    ###############################################################################
-    # Load tokenizer
-    ###############################################################################
-    tokenizer, ntokens, PAD_TOKEN_ID, PAD_TOKEN, BOS_TOKEN_ID = utils.load_tokenizer()
-
-    is_transformer_model = hasattr(model, 'model_type') and model.model_type == 'Transformer'
-
-    ###############################################################################
-    # load data
-    ###############################################################################
-    canary_corpus = CanaryDataset(CANARY, tokenizer)
-    dataloader = DataLoader(dataset=canary_corpus, 
-                            shuffle=False, 
-                            batch_size=args.batch_size, 
-                            collate_fn=canary_corpus.collate)
-
+def get_exposure(model, dataloader, save_json=None):
     ###############################################################################
     # calculate ppl
     ###############################################################################
@@ -143,17 +98,80 @@ if __name__ == "__main__":
     N = len(sorted_ppls)
     canary_rank, canary_ppl = sorted_ppls[CANARY]
 
-    with open(args.outf, 'w') as fh:
-        json.dump(sorted_ppls, fh)
+    if save_json:
+        with open(save_json, 'w') as fh:
+            json.dump(sorted_ppls, fh)
 
-    canary_exposure = math.log(TOTAL_CANDIDATES) - math.log(canary_rank)
+    canary_exposure = math.log(TOTAL_CANDIDATES, 2) - math.log(canary_rank, 2)
     print("canary exposure")
     print(canary_exposure)
     print("canary ranking")
     print(canary_rank)
 
+    return canary_exposure, canary_rank, canary_ppl
+
+def load_model(model_path):
+    with open(model_path, 'rb') as f:
+        model = torch.load(f, map_location=device)
+    model.eval()
+    return model
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 Language Model')
+
+    # Model parameters.
+    parser.add_argument('--checkpoint', '-ckpt', type=str, default='/home/wyshi/privacy/model/nodp/model-LSTM__ebd-200__hid-200__bi-False__nlayer-1__tied-False__ntokens-33278__bs-256__bptt-35__lr-20.0__dp-False.pt',
+                        help='model checkpoint to use')
+    parser.add_argument('--outputf', type=str, 
+                        help='output file for generated text')
+    parser.add_argument('--seed', type=int, default=1111,
+                        help='random seed')
+    parser.add_argument('--batch_size', '-bs', type=int, default=256,
+                        help='batch size')
+    parser.add_argument('--cuda', type=str, default="cuda:0",
+                        help='use CUDA')
+    args = parser.parse_args()
+
+    print(f'output will be saved to {args.outputf}')
+    # Set the random seed manually for reproducibility.
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        if not args.cuda:
+            print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+    device = torch.device(args.cuda)
+
+    ###############################################################################
+    # Load tokenizer
+    ###############################################################################
+    tokenizer, ntokens, PAD_TOKEN_ID, PAD_TOKEN, BOS_TOKEN_ID = utils.load_tokenizer()
 
 
-    
+    ###############################################################################
+    # load data
+    ###############################################################################
+    canary_corpus = CanaryDataset(CANARY, tokenizer)
+    dataloader = DataLoader(dataset=canary_corpus, 
+                            shuffle=False, 
+                            batch_size=args.batch_size, 
+                            collate_fn=canary_corpus.collate)
 
+    ###############################################################################
+    # Load model
+    ###############################################################################    
+    # exposures, ranks, canary_ppls, model_ppls, model_accs = [], [], [], [], []
+    records = []
+    for model_path in tqdm(os.listdir(args.checkpoint)):
+        model = load_model(os.path.join(args.checkpoint, model_path))
+        is_transformer_model = hasattr(model, 'model_type') and model.model_type == 'Transformer'
+        model_ppl, model_acc, epoch_num = float(model_path.split('ppl-')[-1].split('_')[0]), float(model_path.split('acc-')[-1].split('_')[0]), int(model_path.split('epoch-')[-1])
+        canary_exposure, canary_rank, canary_ppl = get_exposure(model, dataloader, save_json=None)    
+        print("model ppl")
+        print(model_ppl)
+        record = [epoch_num, model_ppl, model_acc, canary_exposure, canary_rank, canary_ppl, TOTAL_CANDIDATES]
 
+        records.append(record)
+    records = sorted(records, key = lambda x: x[0])
+    records = pd.DataFrame(records, columns=['epoch', 'model_ppl', 'model_acc', 'canary_exposure', 'canary_rank', 'canary_ppl', 'TOTAL_CANDIDATES'])
+
+    records.to_csv(args.outputf, index=None)
