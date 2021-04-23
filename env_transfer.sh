b4de77dd7a031452b99152518e37fc67e24cca3a
Weiyan Shi <wyshi@ucdavis.edu>
2021-04-23 16:24:23 -0700

    copy privacy engine

--- env_transfer.sh
+++ env_transfer.sh
@@ -3,6 +3,8 @@
 # ./env_transfer.sh
 # pip uninstall torch
 # pip install torch==1.7.1
+# mv /home/wyshi/anaconda3/envs/privacy/lib/python3.8/site-packages/opacus/privacy_engine.py /home/wyshi/anaconda3/envs/privacy/lib/python3.8/site-packages/opacus/privacy_engine_original.py
+# cp privacy_engine.py /home/wyshi/anaconda3/envs/privacy/lib/python3.8/site-packages/opacus/privacy_engine.py
 mkdir -p model
 python -m spacy download en_core_web_sm
 mkdir -p logs/partial_dp/
