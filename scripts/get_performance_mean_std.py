ee87545fca073af46442d0060fddeb1c9d97193e
Weiyan Shi <wyshi@language.cs.ucdavis.edu>
2021-05-16 11:26:52 -0700

    csv

--- scripts/get_performance_mean_std.py
+++ scripts/get_performance_mean_std.py
@@ -3,6 +3,7 @@ from glob import glob
 import os
 BASE_DIR = "data/adjusted_metrics"
 
+# nodp
 paths = """
 model/nodp/20210418/181252/
 model/nodp/20210418/192226/
@@ -11,6 +12,60 @@ model/nodp/20210419/052248/
 model/nodp/20210418/210231/
 """
 
+# full dp
+paths = """
+model/dp/20210425/151315
+model/dp/20210425/151340
+model/dp/20210425/151359
+model/dp/20210425/151417
+model/dp/20210425/151429
+"""
+
+# s dp
+paths = """
+model/partialdp/20210423/221533
+model/partialdp/20210423/221514/
+model/partialdp/20210427/213802
+model/partialdp/20210427/213754
+model/partialdp/20210427/213900
+"""
+
+# # dialog, no dp
+# paths = """
+# """
+
+# # dialog, dp
+# paths = """
+# model/dp/20210502/123625/
+# """
+
+# # dialog, sdp
+# paths = """
+# model/partialdp/20210503/230732
+# model/partialdp/20210503/230450
+# model/partialdp/20210503/230904
+# model/partialdp/20210503/230952
+# model/partialdp/20210504/100507/
+# """
+
+
+# # missed, no dp
+# paths = """
+# model/nodp/20210426/182422
+# model/nodp/20210426/234106
+# model/nodp/20210426/234125
+# model/nodp/20210426/234146
+# model/nodp/20210426/234201
+# """
+
+# # missed, sdp
+# paths = """
+# model/partialdp/20210426/223009
+# model/partialdp/20210427/211240
+# model/partialdp/20210427/211322
+# model/partialdp/20210427/211339
+# model/partialdp/20210427/211411
+# """
 
 paths = paths.split('\n')
 paths = [p for p in paths if p != '']
@@ -35,7 +90,7 @@ print("test metrics: ")
 def print_metric(df, metrics):
     mean = df[metrics].mean()
     std = df[metrics].std()
-    print(f"{metrics}: {mean}, std {std}")
+    print(f"{metrics}: {mean:.2f} $\pm$ {std:.2f}")
     print()
 
 print_metric(dfs_model, 'overall_ppl')
